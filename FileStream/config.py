307f7365dbcafcdbdd72dcf8adb7d6caf8e76bda
Aaki55 <aaki849056@gmail.com>
2024-07-19 10:31:00 +0530

    Update config.py

--- FileStream/config.py
+++ FileStream/config.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Telegram:
-    API_ID = int(env.get("17584090"))
-    API_HASH = str(env.get("3f779654fdd3b9278fd95869aabf4593"))
+    API_ID = int(env.get("28341884"))
+    API_HASH = str(env.get("ca0c9295ce3ec910fd6f49e99970d9a8"))
     BOT_TOKEN = str(env.get("BOT_TOKEN"))
     OWNER_ID = int(env.get('OWNER_ID', '7252430326'))
     WORKERS = int(env.get("WORKERS", "6"))  # 6 workers = 6 commands at once
